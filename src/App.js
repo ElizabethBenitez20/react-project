db5fd6d57a479d83a4f6a3106f33c61f0a1c715a
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2024-05-02 18:38:38 -0400

    The basic structure of the project was created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 className="text-center">Weather App</h1>
+      <Weather />
     </div>
   );
 }
