459222c6c4ebfedb8dda4b034ab5f64d7b57f339
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2024-05-03 15:42:50 -0400

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ export default function App() {
     <div className="App">
       <h1 className="text-center">Weather App</h1>
       <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/ElizabethBenitez20" target="_blank">
+          Elizabeth Benitez
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ElizabethBenitez20/react-project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
