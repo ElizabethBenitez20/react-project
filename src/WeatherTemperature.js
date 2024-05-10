20feb206f96854d44d43e13886bb3c065269a50a
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2024-05-10 16:26:57 -0400

    Unit change button added

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function showFarhrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <button className="button-unit" onClick={showFarhrenheit}>
+            °F
+          </button>
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <button className="button-unit" onClick={showCelsius}>
+            °C
+          </button>{" "}
+          | °F
+        </span>
+      </span>
+    );
+  }
 }
