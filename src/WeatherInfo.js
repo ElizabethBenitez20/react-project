25591f921f31957e45dfe65eb6e8fbce78463e23
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2024-05-10 10:54:34 -0400

    Forecast component added

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,18 +7,18 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
+      <div className="clearfix">
+        <WeatherIcon code={props.data.icon} />
+        <WeatherTemperature celsius={props.data.temperature} />
+      </div>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <WeatherIcon code={props.data.icon} />
-            <WeatherTemperature celsius={props.data.temperature} />
-          </div>
+          <ul>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+          </ul>
         </div>
         <div className="col-6">
           <ul>
